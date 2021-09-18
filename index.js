d099169b10da8e7d399c2c4196a1087081e81709
Suriyaa <suriyaaap13@gmail.com>
2021-09-18 21:15:22 +0530

    create the frontend part of the index page

---
+++ index.js
@@ -0,0 +1,20 @@
+// // populate the index page with the fetched data
+// setTimeout(function () {
+//     console.log(meals.length);
+//     let html="";
+//     console.log(meals[0]);
+//     for(let i = meals[0];i<=0;i++){
+//         html+= 
+//         `<div class="card" style="width: 18rem;">
+//             <img src="" class="card-img-top" alt="${i.strMeal}">
+//             <div class="card-body">
+//             <h5 class="card-title">${i.strMeal}</h5>
+//             <p class="card-text">${i.strInstructions}</p>
+//             <a href="#" class="btn btn-primary"></a>
+//             </div>
+//         </div>`;
+//     }
+    
+//     console.log(html)
+//     document.querySelector('.custom-container').innerHTML= html;
+// }, 350);
