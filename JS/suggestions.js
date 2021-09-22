8f97fccef39a7d06f6ea5582ec395300ba74fcac
Suriyaa <suriyaaap13@gmail.com>
2021-09-22 18:23:42 +0530

    bug fix 1

--- JS/suggestions.js
+++ JS/suggestions.js
@@ -19,11 +19,11 @@ function getMeals(){
 
 // storing the fetched array in local storage
 setTimeout(function(){
-    
+    console.log('I am in',mealAPIData.length);
     let meals_searialized = JSON.stringify(mealAPIData);
     localStorage.setItem("meals",meals_searialized);
     let meals_desearialized = JSON.parse(meals_searialized);
-},870);
+},900);
 
 
 
