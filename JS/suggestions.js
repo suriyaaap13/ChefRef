afc750b74af636ccb7c96fe40dff75c9a081ca98
Suriyaa <76957372+suriyaaap13@users.noreply.github.com>
2022-03-10 00:27:49 +0530

    Minor Bug fixes to make the functions async

--- JS/suggestions.js
+++ JS/suggestions.js
@@ -1,10 +1,18 @@
 //creating meals array to capture all the data from API
 let mealAPIData = new Array();
 var k=0;
+// storing the fetched array in local storage
+let mypromise = function(){
+    console.log('I am in',mealAPIData.length);
+    let meals_searialized = JSON.stringify(mealAPIData);
+    localStorage.setItem("meals",meals_searialized);
+    let meals_desearialized = JSON.parse(meals_searialized);
+}
+
 // calling the function getMeals to fetch the data
 getMeals();
 // getMeals function
-function getMeals(){
+ function getMeals(){
     //fetch the data from API
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=`)
     .then(response => response.json())
@@ -13,20 +21,6 @@ function getMeals(){
         for(let i of data.meals){
             mealAPIData[k++] = i;
         }
-    });
-    
+    })
+    .then(mypromise);
 }
-
-// storing the fetched array in local storage
-setTimeout(function(){
-    console.log('I am in',mealAPIData.length);
-    let meals_searialized = JSON.stringify(mealAPIData);
-    localStorage.setItem("meals",meals_searialized);
-    let meals_desearialized = JSON.parse(meals_searialized);
-},900);
-
-
-
-
-
-    
